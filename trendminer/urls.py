75e965cfabf3453f072a37e3b8191b848fcacb0c
Tim Krones <t.krones@gmx.net>
2012-11-12 10:20:33 +0100

    Implement basic views: home, login, logout

---
+++ trendminer/urls.py
@@ -0,0 +1,29 @@
+"""
+Project: TrendMiner Demo Web Services
+Authors: Christian Federmann <cfedermann@dfki.de>,
+         Tim Krones <tkrones@coli.uni-saarland.de>
+"""
+from django.conf.urls.defaults import include, patterns, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'trendminer.views.home', name='home'),
+    (r'^login/$', 'trendminer.views.login',
+      {'template_name': 'login.html'}),
+
+    (r'^logout/$', 'trendminer.views.logout',
+      {'next_page': '/'}),
+
+
+    # url(r'^trendminer/', include('trendminer.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ trendminer/views.py
@@ -0,0 +1,45 @@
+"""
+Project: TrendMiner Demo Web Services
+Authors: Christian Federmann <cfedermann@dfki.de>,
+         Tim Krones <tkrones@coli.uni-saarland.de>
+"""
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login as _login, logout as _logout
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+
+from trendminer.settings import COMMIT_TAG
+
+
+def home(request):
+    dictionary = {
+      'title': 'TrendMiner Web Services',
+      'commit_tag': COMMIT_TAG,
+    }
+    return render_to_response("home.html", dictionary,
+      context_instance=RequestContext(request))
+
+
+def login(request, template_name):
+    """
+    Renders login view by connecting to django.contrib.auth.views.
+    """
+    if request.user.username:
+        dictionary = {
+          'title': 'TrendMiner Web Services',
+          'commit_tag': COMMIT_TAG,
+          'message': 'You are already logged in as ' \
+            ' <code>"{0}"</code>.'.format(request.user.username),
+        }
+
+        return render(request, 'home.html', dictionary)
+
+    extra_context = {'commit_tag': COMMIT_TAG}
+    return _login(request, template_name, extra_context=extra_context)
+
+
+def logout(request, next_page):
+    """
+    Renders logout view by connecting to django.contrib.auth.views.
+    """
+    return _logout(request, next_page)
