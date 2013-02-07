063bd6fde7c7bfc8b1f288bd1d50c7baa455b564
Tim Krones <t.krones@gmx.net>
2013-02-07 16:35:25 +0100

    Set up basic folder structure for testing

--- trendminer/settings.py
+++ trendminer/settings.py
@@ -144,6 +144,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trendminer',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
---
+++ trendminer/tests/validator_tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+class ValidatorTest(TestCase):
+    def test_ext_validator(self):
+        pass
+
+    def test_size_validator(self):
+        pass
+
+    def test_mime_type_validator(self):
+        pass
+
+    def test_zip_integrity_validator(self):
+        pass
+
+    def test_zip_contents_validator(self):
+        pass
+
+    def test_xml_wf_validator(self):
+        pass
+
+    def test_xml_schema_validator(self):
+        pass
