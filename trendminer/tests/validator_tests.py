b8964eae040720e6d03a2ea8dad3ff10e960da85
Tim Krones <t.krones@gmx.net>
2013-02-12 14:22:08 +0100

    Before testing individual validators, create test user in the DB and log them in; when done, log them out again

--- trendminer/tests/validator_tests.py
+++ trendminer/tests/validator_tests.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client
 
 class ValidatorTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='trendminer-demo', password='trendminer-demo')
+        self.browser = Client()
+        self.browser.login(
+            username=self.user.username, password='trendminer-demo')
+
+    def tearDown(self):
+        self.browser.logout()
+
     def test_ext_validator(self):
         pass
 
